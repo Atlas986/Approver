359a834a8e2811166a108c6e88777e875f025911
readyyyk <mr.sairexer@gmail.com>
2023-12-31 19:39:57 +0300

    [RF] import requests removed, sqlite db added to ignore list

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from src.database import models
 from src.database.database import engine
 from src.views import *
 models.Base.metadata.create_all(bind=engine)
-import requests
+
 
 app = FastAPI()
 
